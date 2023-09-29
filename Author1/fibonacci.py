3eb49d0047eb080af6b662564d1630ae25ecf224
Anna Sargsyan <anna.s@plat.ai>
2023-09-29 11:18:19 +0400

    Change in function heading

--- Author1/fibonacci.py
+++ Author1/fibonacci.py
@@ -4,16 +4,16 @@ if len(sys.argv) < 1:
     print("Usage: fibonacci.py N")
     sys.exit(1)
 
-def fibonacci_rec(n):
+def fib_recursive(n):
     if n <= 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 
 n = int(sys.argv[1])
 
 for i in range(n):
-    print(fibonacci_rec(i))
+    print(fibonacci_recursive(i))
 
